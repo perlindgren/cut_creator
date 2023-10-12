c32907b12bf12033e0d3e4b0a5bc953422367ac9
Per Lindgren <per.lindgren@ltu.se>
2023-10-12 15:12:30 +0200

    plot works

--- examples/plot.rs
+++ examples/plot.rs
@@ -34,9 +34,6 @@ impl Default for MyApp {
     }
 }
 
-use std::ops::Bound::*;
-use std::ops::RangeBounds;
-
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
@@ -46,36 +43,52 @@ impl eframe::App for MyApp {
             ui.horizontal(|ui| {
                 let my_plot = Plot::new("My Plot").height(height).width(width);
 
-                // let's create a dummy line in the plot
-                let graph: Vec<[f64; 2]> = vec![[0.0, 1.0], [2.0, 3.0], [3.0, 2.0], [2.5, 5.0]];
                 my_plot.show(ui, |plot_ui| {
                     if plot_ui.response().clicked() {
                         println!("clicked");
-                    }
 
-                    if let Some(pos) = plot_ui.response().interact_pointer_pos() {
-                        let p2 = plot_ui.pointer_coordinate();
-                        println!("pos {:?}, p2 {:?}", pos, p2);
-                        if let Some(p2) = p2 {
-                            self.knots.push([p2.x, p2.y]);
+                        if let Some(pos) = plot_ui.response().interact_pointer_pos() {
+                            let p2 = plot_ui.pointer_coordinate();
+                            println!("pos {:?}, p2 {:?}", pos, p2);
+
+                            if let Some(p2) = p2 {
+                                let (head, mut tail): (Vec<[f64; 2]>, Vec<_>) =
+                                    self.knots.iter().partition(|k| p2.x < k[0]);
+
+                                println!("head {:?}", head);
+                                println!("tail {:?}", tail);
+
+                                tail.push([p2.x, p2.y]);
+                                tail.extend(head);
+                                self.knots = tail;
+                            }
+
+                            println!("{:?}", self.knots);
+
+                            self.splines = Spline::from_iter(
+                                self.knots
+                                    .iter()
+                                    .map(|p| Key::new(p[0], p[1], Interpolation::CatmullRom)),
+                            )
                         }
                     }
                     plot_ui.line(Line::new(PlotPoints::from(self.knots.clone())));
 
-                    fn f(x: f64) -> f64 {
-                        (x).sin()
-                    };
-                    let plot_points = PlotPoints::from_explicit_callback(f, (0.0..2.0 * PI), 10);
-                    plot_ui.line(Line::new(plot_points));
+                    // fn f(x: f64) -> f64 {
+                    //     (x).sin()
+                    // };
+                    // let plot_points = PlotPoints::from_explicit_callback(f, (0.0..2.0 * PI), 10);
+                    // plot_ui.line(Line::new(plot_points));
 
                     let splines = self.splines.clone();
 
-                    let sample = move |t| {
-                        println!("t {}, sample {:?}", t, splines.sample(t));
-                        splines.sample(t).unwrap()
-                    };
+                    let sample = move |t| splines.sample(t).unwrap();
 
-                    let plot_points = PlotPoints::from_explicit_callback(sample, (2.0..3.0), 10);
+                    let start = self.knots[1][0] + 0.00001; // to ensure we have two knots on either side
+                    let end = self.knots[self.knots.len() - 2][0] - 0.000001;
+                    println!("start {} end {}", start, end);
+
+                    let plot_points = PlotPoints::from_explicit_callback(sample, start..end, 100);
                     plot_ui.line(Line::new(plot_points));
                 });
             });
