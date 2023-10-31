8afd30501ca5a08bc4f3390cdbc6374be1bd8e97
Per Lindgren <per.lindgren@ltu.se>
2023-10-31 19:35:46 +0100

    sinc

--- examples/sinc.rs
+++ examples/sinc.rs
@@ -20,17 +20,50 @@ fn sinc(x: f32) -> f32 {
     }
 }
 
-fn sinc_fir(l: usize) -> Vec<f32> {
-    // let mut fir = vec![];
-    let fs = 48000.0;
-    let step = 0.1;
-    let cutoff = step * fs / 2.0;
-    let limit = 10;
-    for x in 0..limit {
-        println!("{}", x);
-    }
-    unimplemented!()
-}
 fn main() {
-    let _ = sinc_fir(0);
+    let sample_rate = 48000;
+    let time = 1;
+    let attenuation = 0.5;
+    let fr = 440.0;
+
+    let mut sample = vec![];
+    for t in (0..sample_rate * time).map(|x| x as f32 / sample_rate as f32) {
+        sample.push(attenuation * (t * fr * 2.0 * PI).sin());
+    }
+
+    let mut re_sample = vec![];
+
+    let nr_sinc_samples = 10;
+    let first_sinc_sample = nr_sinc_samples / 2;
+
+    let ratio = 10.0;
+    for i in 0..sample_rate * time {
+        // recreate sample at time t
+        let t = i as f32 * ratio;
+        let min_t = t.floor(); // the sample left of the one to re-create
+        let diff = t - min_t;
+
+        let mut s = 0.0;
+        for j in 0..=nr_sinc_samples {
+            let sample_position = min_t as usize - first_sinc_sample;
+            if let Some(in_sample) = sample.get(sample_position) {
+                s += sinc((j as f32 - first_sinc_sample as f32) - diff) * in_sample;
+            }
+        }
+        re_sample.push(s);
+    }
+
+    let spec = hound::WavSpec {
+        channels: 2,
+        sample_rate: 48000,
+        bits_per_sample: 32,
+        sample_format: hound::SampleFormat::Float,
+    };
+
+    let mut writer = hound::WavWriter::create("./audio/sine.wav", spec).unwrap();
+    for (s1, s2) in sample.clone().iter().zip(re_sample) {
+        writer.write_sample(*s1).unwrap();
+        writer.write_sample(s2).unwrap();
+    }
+    writer.finalize().unwrap();
 }
